e406fa5d818de1fb5e4255bafe80ad93345a327d
mindfling <dimendroider@gmail.com>
2022-08-12 17:33:49 +0300

    feat lesson2 prety js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,18 @@
 'use strict';
 
-console.log('Hello crm');
-
-// todo
 const overlay = document.querySelector('.overlay');
-
-// todo
 const modal = overlay.querySelector('.overlay__modal');
 
-// todo button close
-// todo сделаем потом
-const modalClose = modal.querySelector('.modal__close');
-modalClose.addEventListener('click', (event) => {
-  console.log('close');
-  overlay.classList.remove('active');
-});
-
-
-// todo button submit
-// todo сделаем потом
-const modalSubmit = modal.querySelector('.modal__submit');
-
-//! Заголовок
+// Заголовок
 const modalTitle = modal.querySelector('.modal__title');
-//! Форма
+// Форма
 const modalForm = modal.querySelector('.modal__form');
-//! Чекбокс
+// Чекбокс
 const modalCheckbox = modal.querySelector('.modal__checkbox');
-//! Поле рябом с чекбоксом Скидка
+// Поле рябом с чекбоксом Скидка
 const modalInputDiscount = modal.querySelector('.modal__input_discount');
 
+// проверка
 console.log('modalTitle: ', modalTitle);
 console.log('modalForm: ', modalForm);
 console.log('modalCheckbox: ', modalCheckbox);
